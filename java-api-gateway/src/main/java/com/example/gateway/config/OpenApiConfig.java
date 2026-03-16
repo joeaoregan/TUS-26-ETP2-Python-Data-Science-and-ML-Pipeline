525e495d1d3ccbf8e38971fa8ddb2f3f62a64d84
joeaoregan <joe.oregan@gmail.com>
2026-03-16 17:44:56 +0000

    application.props: move api metadata here

--- java-api-gateway/src/main/java/com/example/gateway/config/OpenApiConfig.java
+++ java-api-gateway/src/main/java/com/example/gateway/config/OpenApiConfig.java
@@ -2,15 +2,26 @@ package com.example.gateway.config;
 
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
 
+    @Value("${api.title}")
+    private String apiTitle;
+
+    @Value("${api.description}")
+    private String apiDescription;
+
+    @Value("${api.version}")
+    private String apiVersion;
+
 	@Bean
 	OpenAPI trafficControlApi() {
-		return new OpenAPI().info(new Info().title("AI Traffic Control API")
-				.description("REST API Gateway for RL-based traffic signal control").version("1.0.0"));
+		return new OpenAPI().info(new Info().title(apiTitle)
+				.description(apiDescription).version(apiVersion));
 	}
 }
